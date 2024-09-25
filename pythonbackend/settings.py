72fd0af5d27d483fe8c005c83508e78be8e66dbc
Harrison Itotia <harrisonitotia@gmail.com>
2024-09-25 16:43:12 +0300

    create build_files.sh

--- pythonbackend/settings.py
+++ pythonbackend/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-iyk$n(c=)f8*5nxmoii*w1a0)!@nu!yys+8136lc4kxtwd2ghu'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
