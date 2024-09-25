ab0a2a06deca9981502f04b2a1926cbc3e32a177
Harrison Itotia <harrisonitotia@gmail.com>
2024-09-25 08:16:57 +0300

    create and register models

--- ordersAPI/models.py
+++ ordersAPI/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, related_name='orders', on_delete=models.CASCADE)
+    item = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Order for {self.customer.name} - {self.item}'
