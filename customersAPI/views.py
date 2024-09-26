175bf991ebf432a8f9575d90c8e95ddf0a756695
Harrison Itotia <harrisonitotia@gmail.com>
2024-09-26 20:49:17 +0300

    update views

--- customersAPI/views.py
+++ customersAPI/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets
 from .models import Customer
 from .serializers import CustomerSerializer
 
@@ -7,4 +7,3 @@ from .serializers import CustomerSerializer
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
-    permission_classes = [permissions.IsAuthenticated]
--- ordersAPI/views.py
+++ ordersAPI/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets
 from .models import Order
 from .serializers import OrderSerializer
 
@@ -7,4 +7,3 @@ from .serializers import OrderSerializer
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
-    permission_classes = [permissions.IsAuthenticated]
